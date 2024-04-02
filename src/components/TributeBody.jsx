256e7253ec573fdf67d73ac53eb36404900747f3
DanielGad <adeyemidanielgad@gmail.com>
2024-04-02 21:51:29 +0100

    Updates

--- src/components/TributeBody.jsx
+++ src/components/TributeBody.jsx
@@ -28,7 +28,7 @@ const collectionRef = collection(database, 'tributes');
 
 
 const TributeBody = () => {
-  const { imageUrls, imageProUrls } = useContext(GalleryContext);
+  const { imageUrls, /*imageProUrls*/ } = useContext(GalleryContext);
 
   const [tributes, setTributes] = useState([]);
 
@@ -121,14 +121,14 @@ const TributeBody = () => {
         {tributes.map((tribute) => (
           <div className="tri" key={tribute.id}>
             <div className='tri-cover'>
-            {imageProUrls.map((image, i) => (
+            {/* {imageProUrls.map((image, i) => (
                         <img
                             key={i}
                             src={image}
-                            style={{width: "100%", display: "block", cursor: "pointer"}}
                             alt=""
                         />  
-                    ))}
+                    ))} */}
+                    <img src="/m1.png" alt="" />
               </div>
             <div className='tri-body'>{tribute.body}</div>
               <div className='tri-name'>{tribute.author}</div>
