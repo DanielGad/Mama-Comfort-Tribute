c808d84cd0db79dcd5f6f8afe91917b6a10c238a
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2024-04-15 00:24:36 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Menu = () => {
 
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function toggleMusic() {
+    var music = document.getElementById('bg-music');
+    if (music.paused) {
+      music.play();
+      setIsPlaying(true);
+    } else {
+      music.pause();
+      setIsPlaying(false);
+    }
+  }
 
   return (
 
@@ -9,8 +22,12 @@ const Menu = () => {
       <Link to="/" style={{ textDecoration: 'none' }}><div className='menu-button'>Home</div></Link>
       <Link to="/tributes" style={{ textDecoration: 'none' }}><div className='menu-button'>Tributes</div></Link>
       <Link to="/gallery" style={{ textDecoration: 'none' }}><div className='menu-button'>Gallery</div></Link>
+      <div className="menu-button" onClick={toggleMusic}>
+        {isPlaying ? 'Pause Music' : 'Play Music'}
+      </div>
     </div>
   );
 };
 
 export default Menu;
+
