3b67a5a6db17e0d535d896195e1cbfbbb0e54253
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2024-04-15 01:52:03 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Menu = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const music = document.getElementById('bg-music'); 
+    if (music) { 
+      music.play(); 
+    setIsPlaying(true); 
+    } 
+  }, []);
+
+  function toggleMusic() {
+    var music = document.getElementById('bg-music');
+    if (music) {
+    if (music.paused) {
+      music.play();
+      setIsPlaying(true);
+    } else {
+      music.pause();
+      setIsPlaying(false);
+    } 
+  } else { 
+console.error('Background music not found.'); 
+}
+  }
 
   return (
 
@@ -8,6 +33,9 @@ const Menu = () => {
       <Link to="/" style={{ textDecoration: 'none' }}><div className='menu-button'>Home</div></Link>
       <Link to="/tributes" style={{ textDecoration: 'none' }}><div className='menu-button'>Tributes</div></Link>
       <Link to="/gallery" style={{ textDecoration: 'none' }}><div className='menu-button'>Gallery</div></Link>
+      <div className="menu-button" onClick={toggleMusic}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </div>
     </div>
   );
 };
